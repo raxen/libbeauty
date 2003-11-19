231977ffd8900f8587eefeae5b2f951255bed715
James Courtier-Dutton <James@superbug.co.uk>
2003-11-19 23:07:07 +0000

    Fix a link.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -5,7 +5,7 @@ include 'header.php'
 <table width="90%" class="centered">
 <H2>Revenge 
 </H2>
-<P>This site is dedicated to reverse endineering and decompiling. Anyone interested in things like a decompiler will find useful information here. For some more details, please look at the <A HREF="faq.html">FAQ</A>
+<P>This site is dedicated to reverse endineering and decompiling. Anyone interested in things like a decompiler will find useful information here. For some more details, please look at the <A HREF="faq.php">FAQ</A>
 .</P>
 <P><A HREF="ftp://anonymous@ftp.berlios.de/pub/revenge/reference/paycheck_m480.mov">A
 demonstration of what we might be able to do.</A> :-)</P>
