e78538b4236b513bfae547ad6b5a7477b184d4db
James Courtier-Dutton <James@superbug.co.uk>
2003-11-21 18:44:48 +0000

    Update with new tool details.

--- www/htdocs/faq.php
+++ www/htdocs/faq.php
@@ -14,7 +14,8 @@ include 'header.php'
 <BLOCKQUOTE>The first planned piece of software is going to be called
 &quot;revenge&quot;. It is only in planning at the moment. See the
 <A HREF="revenge.php">User Guide</A> for the plans so far.</BLOCKQUOTE>
-<BLOCKQUOTE><BR><BR>
-</BLOCKQUOTE>
+<H4>3. Other tools being planned. 
+</H4>
+<BLOCKQUOTE>Another tool that will need to be developed is a way to decompress executable files that have run-time decompression software in them. Some wine32 .dll files are compressed, and when loaded create 3 segments in memory and also contain the symbols of all the functions contained in the .dll file. Segment 1 is designated empty space with a certain size. It is this segment that all the function symbols point to. Segment 2 is data, and Segment 3 is some machine code. The Machine code takes the data in segment 2 and expands it into segment 1, so that after expandsion, the function symbols all point to the correct places with valid code in them. We will need to create a tool that takes the original .dll file, and expands it and then saves it again in a .dll format that is still runnable and functions exactly the same as the compressed version.
 </body>
 </html>
