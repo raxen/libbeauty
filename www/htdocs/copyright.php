8171728121fe19bd58255e89c981880a34c715cf
James Courtier-Dutton <James@superbug.co.uk>
2004-05-25 14:26:33 +0000

    Update copyright message.

--- www/htdocs/copyright.php
+++ www/htdocs/copyright.php
@@ -1,3 +1,3 @@
 <center>
-<font size="1" face="arial">Copyright (c) 2003 James Courtier-Dutton. All Rights Reserved.</font>
+<font size="1" face="arial">Copyright (c) 2004 James Courtier-Dutton. All Rights Reserved.</font>
 </center>
