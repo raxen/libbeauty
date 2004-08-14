52909efae9699591e4d379a33d96223c941a6667
James Courtier-Dutton <James@superbug.co.uk>
2004-08-14 23:00:14 +0000

    Spelling corrections.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -5,11 +5,11 @@ include 'header.php'
 <table width="90%" class="centered">
 <H2>Revenge 
 </H2>
-<P>This site is dedicated to reverse endineering and decompiling. Anyone interested in things like a decompiler will find useful information here. For some more details, please look at the <A HREF="faq.php">FAQ</A>
+<P>This site is dedicated to reverse engineering and decompiling. Anyone interested in things like a decompiler will find useful information here. For some more details, please look at the <A HREF="faq.php">FAQ</A>
 .</P>
 <P><A HREF="ftp://anonymous@ftp.berlios.de/pub/revenge/reference/paycheck_m480.mov">A
 demonstration of what we might be able to do.</A> :-)</P>
-<P><A HREF="http://www.itee.uq.edu.au/~cristina/dcc.html">Crinstina's
+<P><A HREF="http://www.itee.uq.edu.au/~cristina/dcc.html">Cristina's
 page is good start for any budding reverse engineer.</A> Particular
 interest is the <A HREF="http://www.itee.uq.edu.au/~cristina/dcc/decompilation_thesis.ps.gz">PhD
 Thesis</A>. (<A HREF="reference/decompilation_thesis.ps.gz">A local
