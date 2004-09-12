461d6f4dfd0f9485457b193086ef26ce9f30dc39
James Courtier-Dutton <James@superbug.co.uk>
2004-09-12 12:57:06 +0000

    Clean up.

--- revenge/src/input/instruction_decoder/ia32/dis.c
+++ revenge/src/input/instruction_decoder/ia32/dis.c
@@ -28,40 +28,6 @@
 
 #include "dis.h"
 
-instructions_t instructions;
-uint8_t inst[]={0x57,
-	0x56,
-	0x53,
-	0x83, 0xec, 0x18,
-	0x8b, 0x74, 0x24, 0x28,
-	0x8d, 0x44, 0x24, 0x14,
-	0x8b, 0x5c, 0x24, 0x2c,
-	0x8d, 0x7e, 0x48,
-	0x50,
-	0x57,
-	0xe8, 0xfc, 0xff, 0xff, 0xff,
-	0x58,
-	0x5a,
-	0x0f, 0xb7, 0x44, 0x24, 0x30,
-	0xc1, 0xe3, 0x10,
-	0x09, 0xc3,
-	0x66, 0x8b, 0x56, 0x08,
-	0x89, 0xd8,
-	0xef, 
-	0x83, 0xc2, 0x04,
-	0xed,
-	0xff, 0x74, 0x24, 0x14,
-	0x57,
-	0x89, 0xc3,
-	0xe8, 0xfc, 0xff, 0xff, 0xff,
-	0x83, 0xc4, 0x20,
-	0x89, 0xd8,
-	0x5b,
-	0x5e,
-	0x5f,
-	0xc3
-};
-
 /* Little endian */
 uint32_t getbyte(uint8_t *address) {
 	uint32_t result;
@@ -845,62 +811,5 @@ int disassemble(instructions_t *instructions, uint8_t *inst) {
 		break;
 	}
 
-	return 0;
-}
-
-
-
-int main(int argc, char *argv[]) {
-//8b 74 24 28
-	int n=0;
-	int offset=0;
-	instruction_t *instruction;
-	printf("sizeof inst=%d\n",sizeof(inst));
-	for (n=0;n<sizeof(inst);n++) {
-		printf(" 0x%02x",inst[n]);
-	}
-	printf("\n");
-	instructions.bytes_used=0;
-	for(offset=0;offset<sizeof(inst);offset+=instructions.bytes_used) {
-		instructions.instruction_number=0;
-		instructions.bytes_used=0;
-	        disassemble(&instructions, inst+offset);
-		printf("bytes used=%d\n",instructions.bytes_used);
-		for (n=0;n<instructions.bytes_used;n++) {
-			printf(" 0x%02x",inst[n+offset]);
-		}
-		printf("\n");
-		printf("instruction_number=%d\n",instructions.instruction_number);
-		for (n=0;n<instructions.instruction_number;n++) {
-			instruction = &instructions.instruction[n];	
-			printf("Instruction %d:%s",
-				n,
-				opcode_table[instruction->opcode]);
-			if (instruction->srcA.indirect) {
-				printf(" %s[%s0x%x],",
-					size_table[instruction->srcA.size],
-					store_table[instruction->srcA.store],
-					instruction->srcA.value);
-			} else {
-				printf(" %s%s0x%x,",
-				size_table[instruction->srcA.size],
-				store_table[instruction->srcA.store],
-				instruction->srcA.value);
-			}
-			if (instruction->dstA.indirect) {
-				printf(" %s[%s0x%x]\n",
-					size_table[instruction->dstA.size],
-					store_table[instruction->dstA.store],
-					instruction->dstA.value);
-			} else {
-				printf(" %s%s0x%x\n",
-				size_table[instruction->dstA.size],
-				store_table[instruction->dstA.store],
-				instruction->dstA.value);
-			}
-		}
-	}
-
-	printf("test1\n");
 	return 0;
 }
