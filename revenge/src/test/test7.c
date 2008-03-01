c23c61a231b21d12b44a1016d7335e18015b7c87
James Courtier-Dutton <James@superbug.co.uk>
2008-03-01 20:46:41 +0000

    Add test7.c This tests the "if" statement with a simple forward goto.

---
+++ revenge/src/test/test7.c
@@ -0,0 +1,12 @@
+/* A very simple function to test an if statement. */
+
+int test7(int value1 );
+
+int test7(int value1 ) {
+	int local1;
+	if (value1 > 5) {
+		local1 = value1 + 10;
+	}
+	return local1;
+}
+
