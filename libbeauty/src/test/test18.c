7c7a04b68886b44e7430e8e9bc4b746ab7cee620
James Courtier-Dutton <James@superbug.co.uk>
2009-10-21 19:27:34 +0000

    Add new tests for multiple function params.

---
+++ libbeauty/src/test/test18.c
@@ -0,0 +1,17 @@
+/* To test one function call with many parameters in a way that one can automate the test. */
+/* Input values should range from 0-7 */
+#include <stdint.h>
+
+int test17 ( int64_t value1, int64_t value2, int64_t value3, int64_t value4, int64_t value5, int64_t value6, int64_t value7, int64_t value8 ) {
+	int64_t tmp = 0;
+	tmp = (tmp * 8) + value1;
+	tmp = (tmp * 8) + value2;
+	tmp = (tmp * 8) + value3;
+	tmp = (tmp * 8) + value4;
+	tmp = (tmp * 8) + value5;
+	tmp = (tmp * 8) + value6;
+	tmp = (tmp * 8) + value7;
+	tmp = (tmp * 8) + value8;
+	return tmp;
+}
+
