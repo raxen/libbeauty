d0b59407461a622d2af3dbf90028935a241116be
James Courtier-Dutton <James@superbug.co.uk>
2009-12-12 16:32:57 +0000

    Ensure this test preserves the order of the params.

--- libbeauty/src/test/test30.c
+++ libbeauty/src/test/test30.c
@@ -1,7 +1,19 @@
-/* To test one function calling another in a single .o file. */
+/* To test one function with many args. */
 #include <stdint.h>
 
-int test30 ( int64_t value1, int64_t value2, int64_t value3, int64_t value4, int64_t value5, int64_t value6, int64_t value7, int64_t value8 ) {
-	return value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8;
+int test30 ( int64_t value1, int64_t value2, int64_t value3, int64_t value4, int64_t value5, int64_t value6, int64_t value7, int64_t value8 )
+{
+	int64_t local;
+	local = 0;
+	local += value1;
+	local += value2;
+	local += value3;
+	local += value4;
+	local += value5;
+	local += value6;
+	local += value7;
+	local += value8;
+	
+	return local;
 }
 
