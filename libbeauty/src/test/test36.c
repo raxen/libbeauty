141dc3fbc0f69a78f11419b1ef7306fc0dafaecc
James Courtier-Dutton <James@superbug.co.uk>
2011-11-01 22:02:59 +0000

    Add test36.c Test multiple paths through function.

---
+++ libbeauty/src/test/test36.c
@@ -0,0 +1,16 @@
+/* A very simple function to split program flow. */
+
+int main()
+{
+	int var1;
+	int var2;
+	var1 = 0;
+	var2 = 0;
+	if (var1 == 2) {
+		var2 ++;
+	} else {
+		var2 = var2 + 4;
+	}
+	var2 ++;
+}	
+
