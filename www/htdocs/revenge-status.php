023edaaf22eb8fc49921f603096eac73b01ff278
James Courtier-Dutton <James@superbug.co.uk>
2012-03-27 13:03:17 +0100

    Update status, using GIT and not SVN.

--- www/htdocs/revenge-status.php
+++ www/htdocs/revenge-status.php
@@ -10,7 +10,7 @@ include 'header.php'
 
 <P> The revenge software application developement has already started. We have built the BFD (binary file decoder, program loader) part and also the x86 to RTL instruction decoder. This will take x86 instruction hex code and convert it directly into RTL instructions. This will be our equivalent of disassembly. We can currently disassemble about 50% of the x86 instructions. The latest svn currently passes the first 13 test sample programs. It produces C source code from the binary .o file. 
 </P>
-<P> The current source code can be found in the SVN repository under the "trunk->revenge". <A HREF="http://developer.berlios.de/svn/?group_id=1165">See svn repository for latest test version.</A>
+<P> The current source code can be found in the GIT repository under the "git://git.berlios.de/revenge". <A HREF="https://developer.berlios.de/git/?group_id=1165">See GIT repository for latest test version.</A>
 </P>
 <P> The memory module has been written. The BFD will load the program into memory in much the same way that the linux kernel loads a program into memory so that the CPU can execute it. Any extra details that might happen to be in the binary headers (e.g. debug info), will be loaded into a separate table, so that other modules can use them as they see fit. The BFD and memory module will not execute any program code.
 </P>
@@ -26,7 +26,7 @@ include 'header.php'
 </P>
 <P> Currently, SSA form is being introduced. The difficult part of handling the renaming of labels has been started.
 </P>
-<P> This page was last updated on 10th October 2009
+<P> This page was last updated on 20th MarchOctober 2012
 </P>
 </table>
 </table>
